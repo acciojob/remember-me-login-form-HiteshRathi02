ca805875b20e4d1e86b8b64a03e5cd100670167d
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-06-26 18:52:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-//your JS code here. If required.
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const remember = document.getElementById("checkbox");
+const submit = document.getElementById("submit");
+const existing = document.getElementById("existing");
+
+function submission(e) {
+  e.preventDefault();
+  
+  const user = username.value.trim();
+  const pass = password.value;
+  const rememberMe = remember.checked;
+
+  username.value = "";
+  password.value = "";
+  remember.checked = false;
+
+  if(rememberMe) {
+    localStorage.setItem("username", user);
+    localStorage.setItem("password", pass);
+    existing.style.display = "block";
+  } else {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+    existing.style.display = "none";
+  }
+
+  alert(`Logged in as ${user}`)
+}
+
+existing.addEventListener("click", () => {
+  const storedUser = localStorage.getItem("username");
+    alert(`Logging in as ${storedUser}`);
+  })
+
+window.onload = () => {
+  const storedUser = localStorage.getItem("username");
+  const storedPass = localStorage.getItem("password");
+
+  if(storedUser && storedPass) {
+    existing.style.display = "block";
+  }
+  else{
+    existing.style.display = "none";
+  }
+
+}
